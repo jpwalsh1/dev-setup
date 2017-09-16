96299519e049feab0482bf817dc4daa7daa3386b
Jim Walsh <jpwalsh1@gmail.com>
2017-09-15 22:05:48 -0400

    jpw: key repeat update

--- osx.sh
+++ osx.sh
@@ -188,7 +188,8 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+# Tweaked so Jim doesnt go insane
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 # Set language and text formats
 #defaults write NSGlobalDomain AppleLanguages -array "en"
